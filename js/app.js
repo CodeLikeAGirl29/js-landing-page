9adb9d9c7973b5d08144e8d065f3293d9bf72b68
Lindsey Howard <miss_leading009@yahoo.com>
2022-03-26 10:05:55 -0400

    fixed all errors

--- js/app.js
+++ js/app.js
@@ -14,137 +14,65 @@
  */
 
 /**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global letiables
+ * Define Global Variables
  *
  */
 
-const sections = document.querySelectorAll("[data-nav]");
-const navList = document.querySelector("#navbar__list");
-
-let newLink;
+const navElements = document.querySelectorAll("section");
+const navList = document.getElementById("navbar__list");
 
 /**
- * End Global letiables
- * Begin Main Functions
- *
+ * End Global Variables
  */
 
-function navSection() {
-	for (let i = 0; i < sections.length; i++) {
-		let navItem = document.createElement("li");
+// Building the navbar
+navElements.forEach((el) => {
+	const navlistElement = `<li class='menu__link ${el.className}' data-link=${el.id}><a href="#${el.id}">${el.dataset.nav}</li>`;
+	navList.insertAdjacentHTML("beforeend", navlistElement);
+});
 
-		navList.append(navItem);
+// scroll to section
+navList.addEventListener("click", (e) => {
+	e.preventDefault();
+	const parent = e.target.hasAttribute("data-link")
+		? e.target
+		: e.target.parentElement;
+	const elementToScrollTo = document.getElementById(parent.dataset.link);
+	elementToScrollTo.scrollIntoView({ block: "end", behavior: "smooth" });
+});
 
-		newLink = document.createElement("a");
+// setting the active class to section
+const callback = (entries) => {
+	entries.forEach((entry) => {
+		const navListElement = document.querySelector(
+			`.menu__link[data-link='${entry.target.id}']`
+		);
+		const section = document.getElementById(entry.target.id);
 
-		navItem.append(newLink);
+		if (entry && entry.isIntersecting) {
+			navListElement.classList.add("active");
+			section.classList.add("active");
+		} else {
+			if (navListElement.classList.contains("active")) {
+				navListElement.classList.remove("active");
+			}
 
-		newLink.innerHTML = sections[i].getAttribute("data-nav");
-
-		newLink.classList.add("menu__link");
-		newLink.style.cursor = "pointer";
-
-		newLink.setAttribute("href", "#section" + (i + 1));
-
-		newLink.setAttribute("data-link", sections[i].getAttribute("data-nav"));
-	}
-}
-
-/*
- * End Main Functions
- */
-
-// build the nav
-
-navSection();
-
-// Add class 'active' to section when near top of viewport
-
-const links = document.querySelectorAll(".nav-link");
-
-const navLi = document.querySelectorAll("nav .container ul li");
-window.onscroll = () => {
-	let current = "";
-
-	sections.forEach((section) => {
-		const sectionTop = section.offsetTop;
-		if (pageYOffset >= sectionTop - 60) {
-			current = section.getAttribute("id");
-		}
-	});
-
-	navLi.forEach((li) => {
-		li.classList.remove("active");
-		if (li.classList.contains(current)) {
-			li.classList.add("active");
+			if (section.classList.contains("active")) {
+				section.classList.remove("active");
+			}
 		}
 	});
 };
 
-changeLinkState();
-window.addEventListener("scroll", changeLinkState);
-// Scroll to section on link click
+// options
+const options = {
+	root: null,
+	rootMargin: "0px",
+	threshold: 0.6,
+};
 
-// Scroll to anchor ID using scrollTO event
-
-const linksList = document.querySelectorAll("a[href^='#section']");
-
-linksList.forEach(function (link) {
-	link.addEventListener("click", () => {
-		linksList.forEach((link) => {
-			if (link.classList.contains("active-link")) {
-				link.classList.remove("active-link");
-			}
-		});
-
-		link.classList.add("active-link");
-
-		let ref = link.href.split("#section");
-		ref = "#section" + ref[1];
-
-		// Scroll to section on link click
-
-		window.scroll({
-			behavior: "smooth",
-			left: 0,
-
-			top: document.querySelector(ref).offsetTop,
-		});
-	});
+// active callback check
+const observer = new IntersectionObserver(callback, options);
+navElements.forEach((el) => {
+	observer.observe(document.getElementById(el.id));
 });
-
-// scroll to top button
-document.addEventListener("scroll", handleScroll);
-// get a reference to our predefined button
-let scrollToTopBtn = document.querySelector(".scrollToTopBtn");
-
-function handleScroll() {
-	let scrollableHeight =
-		document.documentElement.scrollHeight -
-		document.documentElement.clientHeight;
-	let GOLDEN_RATIO = 0.5;
-
-	if (document.documentElement.scrollTop / scrollableHeight > GOLDEN_RATIO) {
-		//show button
-		if (!scrollToTopBtn.classList.contains("showScrollBtn"))
-			scrollToTopBtn.classList.add("showScrollBtn");
-	} else {
-		//hide button
-		if (scrollToTopBtn.classList.contains("showScrollBtn"))
-			scrollToTopBtn.classList.remove("showScrollBtn");
-	}
-}
-
-scrollToTopBtn.addEventListener("click", scrollToTop);
-
-function scrollToTop() {
-	window.scrollTo({
-		top: 0,
-		behavior: "smooth",
-	});
-}
