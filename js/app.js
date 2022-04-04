f75a4243be88caf6a4fcbc32db57c2960ca941cf
Lindsey Howard <miss_leading009@yahoo.com>
2022-04-03 19:04:24 -0500

    reconstructed my js

--- js/app.js
+++ js/app.js
@@ -14,65 +14,67 @@
  */
 
 /**
- * Define Global Variables
+ * Define Global elements
  *
  */
 
-const navElements = document.querySelectorAll("section");
-const navList = document.getElementById("navbar__list");
+const navList = document.querySelector("#navbar__list");
+const sections = document.querySelectorAll("section");
+const selected = document.querySelectorAll("section");
 
 /**
  * End Global Variables
+ * Start Helper Functions
+ *
  */
 
-// Building the navbar
-navElements.forEach((el) => {
-	const navlistElement = `<li class='menu__link ${el.className}' data-link=${el.id}><a href="#${el.id}">${el.dataset.nav}</li>`;
-	navList.insertAdjacentHTML("beforeend", navlistElement);
-});
-
-// scroll to section
-navList.addEventListener("click", (e) => {
-	e.preventDefault();
-	const parent = e.target.hasAttribute("data-link")
-		? e.target
-		: e.target.parentElement;
-	const elementToScrollTo = document.getElementById(parent.dataset.link);
-	elementToScrollTo.scrollIntoView({ block: "end", behavior: "smooth" });
-});
-
-// setting the active class to section
-const callback = (entries) => {
-	entries.forEach((entry) => {
-		const navListElement = document.querySelector(
-			`.menu__link[data-link='${entry.target.id}']`
-		);
-		const section = document.getElementById(entry.target.id);
-
-		if (entry && entry.isIntersecting) {
-			navListElement.classList.add("active");
-			section.classList.add("active");
+//check if the element is in view or not
+function inViewport(sec) {
+	let distance = sec.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.bottom <=
+			(window.innerHeight + 400 || document.documentElement.clientHeight)
+	);
+}
+// navigation bar
+function buildNav(sectionsArray) {
+	let listFregmant = document.createDocumentFragment();
+	sections.forEach((s) => {
+		let li = document.createElement("li");
+		li.innerHTML = `
+			 <a href="#${s.id}" class="menu__link" data-nav="${s.dataset.nav}">${s.dataset.nav}</a>
+		 `;
+		listFregmant.appendChild(li);
+	});
+	navList.appendChild(listFregmant);
+}
+// adding active classes
+function showViewed() {
+	sections.forEach((s) => {
+		let selectedNav = document.querySelector(`[data-nav="${s.dataset.nav}"]`);
+		if (inViewport(s)) {
+			selectedNav.classList.add("selected");
+			s.classList.add("your-active-class");
 		} else {
-			if (navListElement.classList.contains("active")) {
-				navListElement.classList.remove("active");
-			}
-
-			if (section.classList.contains("active")) {
-				section.classList.remove("active");
-			}
+			selectedNav.classList.remove("selected");
+			s.classList.remove("your-active-class");
 		}
 	});
-};
+}
 
-// options
-const options = {
-	root: null,
-	rootMargin: "0px",
-	threshold: 0.6,
-};
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
 
-// active callback check
-const observer = new IntersectionObserver(callback, options);
-navElements.forEach((el) => {
-	observer.observe(document.getElementById(el.id));
-});
+// build the nav
+buildNav(sections);
+document.addEventListener("scroll", showViewed);
+
+/**
+ * End Main Functions
+ *
+ *
+ */
