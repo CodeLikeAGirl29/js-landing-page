3d561ba3f15291b298a39a936132bdc2585fec99
Lindsey Howard <miss_leading009@yahoo.com>
2022-06-13 09:08:48 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,67 +14,133 @@
  */
 
 /**
- * Define Global elements
- *
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
  */
 
-const navList = document.querySelector("#navbar__list");
+/**
+ * Define Global Variables
+ */
 const sections = document.querySelectorAll("section");
-const selected = document.querySelectorAll("section");
+let navList = document.querySelector("#navbar__list");
+let listItems = [];
+const headerNavBar = document.querySelector(".page__header");
+const scrollTopBtn = document.querySelector(".btn-scrl-top")
+// nav animation to be used for set time out and clear time out 
+let navAnimation;
+let sectionHeadings = document.querySelectorAll("section h2");
+
 
 /**
  * End Global Variables
- * Start Helper Functions
- *
  */
 
-//check if the element is in view or not
-function inViewport(sec) {
-	let distance = sec.getBoundingClientRect();
-	return (
-		distance.top >= 0 &&
-		distance.bottom <=
-			(window.innerHeight + 400 || document.documentElement.clientHeight)
-	);
-}
-// navigation bar
-function buildNav(sectionsArray) {
-	let listFregmant = document.createDocumentFragment();
-	sections.forEach((s) => {
-		let li = document.createElement("li");
-		li.innerHTML = `
-			 <a href="#${s.id}" class="menu__link" data-nav="${s.dataset.nav}">${s.dataset.nav}</a>
-		 `;
-		listFregmant.appendChild(li);
-	});
-	navList.appendChild(listFregmant);
-}
-// adding active classes
-function showViewed() {
-	sections.forEach((s) => {
-		let selectedNav = document.querySelector(`[data-nav="${s.dataset.nav}"]`);
-		if (inViewport(s)) {
-			selectedNav.classList.add("selected");
-			s.classList.add("your-active-class");
-		} else {
-			selectedNav.classList.remove("selected");
-			s.classList.remove("your-active-class");
-		}
-	});
-}
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
 
 // build the nav
-buildNav(sections);
-document.addEventListener("scroll", showViewed);
 
-/**
- * End Main Functions
- *
- *
- */
+sections.forEach((section) => {
+    let sectionNavData = section.getAttribute("data-nav");
+    let item = `<li class="menu__link" data-section="${sectionNavData}">${sectionNavData}</li>`;
+    listItems.push(item);
+});
+navList.innerHTML = listItems.join("\n");
+let navItems = document.querySelectorAll(".menu__link");
+
+// Add class 'active' to section when near top of viewport
+
+document.addEventListener("scroll", () => {
+    headerNavBar.classList.remove("hidden");
+    clearTimeout(navAnimation);
+    // Hide fixed navigation bar while not scrolling
+    navAnimation = setTimeout(() => headerNavBar.classList.add("hidden"), 1500);
+    // setting active section   
+    sections.forEach(section => {
+        if (section.getBoundingClientRect().top <= 200 &&
+            section.getBoundingClientRect().bottom >= 200) {
+            section.classList.add("your-active-class");
+            // setting active tab
+            navItems.forEach(item => {
+                if (item.getAttribute("data-section") === section.getAttribute("data-nav")) {
+                    item.classList.add("active");
+                } else item.classList.remove("active");
+            });
+        } else section.classList.remove("your-active-class");
+    });
+    // remmove all active nav links status and nav animation at top
+    if (window.scrollY < 200) {
+        navItems.forEach(item => item.classList.remove("active"));
+        clearTimeout(navAnimation);
+    };
+    //  show scroll to top btn
+    if (window.scrollY > 300) {
+        scrollTopBtn.classList.add("show")
+    } else scrollTopBtn.classList.remove("show")
+});
+
+// scrolling by nav links
+navItems.forEach(item => {
+    item.addEventListener("click",(event => {
+        event.preventDefault();
+        let sectionSelected = event.target.getAttribute("data-section");
+        let sec = document.querySelector(`[data-nav="${sectionSelected}"]`);
+        sec.scrollIntoView({behavior: "smooth"})  
+    }))
+}) 
+
+
+// scroll to top buttun
+
+scrollTopBtn.addEventListener('click', event => {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+
+// append collapse button to sections heading 
+function generateCollaseIcon(id) {
+
+    /* <button id="btn-(id)" class="collapse-icon"><img src="minus.png" alt="collapse"></button> */
+
+    let collapseIcon = document.createElement("button");
+    collapseIcon.classList.add("collapse-icon");
+    if (id % 2) collapseIcon.classList.add("collapse-icon__left");
+    collapseIcon.setAttribute("id", `sectionBtn-${id + 1}`);
+    let collapseIconImg = document.createElement("img");
+    collapseIconImg.setAttribute("src", "./images/minus.png");
+    collapseIconImg.setAttribute("alt", "collapse icon");
+    collapseIcon.appendChild(collapseIconImg);
+    return collapseIcon
+}
+
+sectionHeadings.forEach((head, index) => {
+    let collapseIcon = generateCollaseIcon(index);
+    head.appendChild(collapseIcon);
+})
+
+// collapse Icons 
+
+let collapseIcons = document.querySelectorAll(".collapse-icon")
+
+collapseIcons.forEach(icon => {
+    let isCollased = true;
+    icon.addEventListener("click", event => {
+        event.preventDefault();
+        let btnId = event.target.id;
+        if (isCollased) {
+            document.querySelector(`#${btnId} img`).setAttribute("src", "./images/add-button.png")
+            isCollased = false
+        } else {
+            document.querySelector(`#${btnId} img`).setAttribute("src", "./images/minus.png")
+            isCollased = true
+        }
+        btnNum = btnId.split("-")[1];
+        sections.forEach(section => {
+            if (section.id === `section${btnNum}`) {
+                section.classList.toggle("hidden")
+            }
+        })
+    })
+})
